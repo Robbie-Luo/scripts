11342f93a0925acf5f9d83197de2548a73b18be6
Robbie-Luo <luobutang@gmail.com>
2022-07-12 20:16:49 +0800

    Fix llvm config

--- compile_blender.sh
+++ compile_blender.sh
@@ -24,7 +24,8 @@ compile cmake v3.23.2 https://github.com/Kitware/CMake.git
 compile_release oneTBB v2021.5.0 https://github.com/oneapi-src/oneTBB.git 
 compile_py python v3.10.5 https://github.com/python/cpython.git
 compile_boost boost 1.78.0 https://github.com/boostorg/boost.git
-
+# compile_ispc
+compile_llvm llvm-project 12.0.1 https://github.com/llvm/llvm-project.git 
 compile_deps ptex v2.4.1 https://github.com/wdas/ptex.git
 compile_deps libjpeg-turbo 2.1.3 https://github.com/libjpeg-turbo/libjpeg-turbo.git
 compile_deps libheif v1.12.0 https://github.com/strukturag/libheif.git
@@ -45,6 +46,8 @@ compile ocio v2.1.2 https://github.com/AcademySoftwareFoundation/OpenColorIO.git
 oiio_d="-D STOP_ON_WARNING=OFF -D USE_QT=OFF -D USE_PYTHON=OFF -D USE_FFMPEG=OFF -D USE_OPENVDB=OFF -D BUILD_TESTING=OFF -D OIIO_BUILD_TESTS=OFF -D OIIO_BUILD_TOOLS=OFF"
 compile oiio v2.3.16.0 https://github.com/OpenImageIO/oiio.git "$oiio_d"
 
+
+
 osl_d="-D CMAKE_CXX_STANDARD=14 -D USE_LLVM_BITCODE=OFF -D USE_PARTIO=OFF -D USE_PYTHON=OFF -D USE_QT=OFF -D INSTALL_DOCS=OFF"
 if [ $(uname -m) != "aarch64" ]; then
     osl_d="$osl_d -D USE_SIMD=sse2"
@@ -52,9 +55,6 @@ fi
 compile osl Release-1.11.17.0 https://github.com/AcademySoftwareFoundation/OpenShadingLanguage.git "$osl_d"
 compile osd v3_4_4 https://github.com/PixarAnimationStudios/OpenSubdiv.git "-DNO_TBB=ON -DNO_DOC=ON"
 
-
-compile_ispc
-# compile_llvm llvm-project 12.0.1 https://github.com/llvm/llvm-project.git 
 compile_release embree v3.13.3 https://github.com/embree/embree.git "-DEMBREE_RAY_PACKETS=ON"
 
 if [ $(uname -m) == "aarch64" ]; then
--- compiler.sh
+++ compiler.sh
@@ -122,21 +122,24 @@ function compile_ispc()
     prepare_build llvm-ispc llvmorg-13.0.1 https://github.com/llvm/llvm-project.git 
 
     PREFIX=$INST_DIR/llvm-ispc/13.0.1
-    cd $LIB_DIR/llvm-ispc
-    for patch in $(ls $LIB_DIR/ispc/llvm_patches | grep 13_0)
-    do 
-        git apply $LIB_DIR/ispc/llvm_patches/$patch
-    done
-    git status
-    cd $LIB_DIR/llvm-ispc/build
-    cmake  -G "Ninja" -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;openmp" -DLLVM_ENABLE_DUMP=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_INSTALL_UTILS=ON  -DLLVM_TARGETS_TO_BUILD=AArch64\;ARM\;X86  -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly  ../llvm
-    ninja install
+    if [ ! -d $PREFIX ];then
+        cd $LIB_DIR/llvm-ispc
+        git reset HEAD --hard
+        for patch in $(ls $LIB_DIR/ispc/llvm_patches | grep 13_0)
+        do 
+            git apply $LIB_DIR/ispc/llvm_patches/$patch
+        done
+        cd $LIB_DIR/llvm-ispc/build
+        cmake  -G "Ninja" -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;openmp" -DLLVM_ENABLE_DUMP=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_INSTALL_UTILS=ON  -DLLVM_TARGETS_TO_BUILD=AArch64\;ARM\;X86  -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly  ../llvm
+        ninja install
+    fi
     update_path $PREFIX
 
     PREFIX=$INST_DIR/ispc/v1.18.0
     cd $LIB_DIR/ispc/build
     cmake  -G "Ninja" .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
     ninja install
+    update_path $PREFIX
 }
 
 function compile_oidn()
